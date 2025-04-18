70d2b1ad4d9556aff50a7c31252e0c468cb131e8
thanhdev1710 <chithanh171004@gmail.com>
2025-04-18 20:32:43 +0700

    update seo

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,43 +5,43 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
   return [
     {
-      url: "https://flamee.com",
+      url: "https://flamee.vn",
       lastModified,
       changeFrequency: "daily",
       priority: 1.0,
     },
     {
-      url: "https://flamee.com/about",
+      url: "https://flamee.vn/about",
       lastModified,
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://flamee.com/signin",
+      url: "https://flamee.vn/signin",
       lastModified,
       changeFrequency: "yearly",
       priority: 0.4,
     },
     {
-      url: "https://flamee.com/signup",
+      url: "https://flamee.vn/signup",
       lastModified,
       changeFrequency: "yearly",
       priority: 0.4,
     },
     {
-      url: "https://flamee.com/feed",
+      url: "https://flamee.vn/feed",
       lastModified,
       changeFrequency: "hourly",
       priority: 0.9,
     },
     {
-      url: "https://flamee.com/terms",
+      url: "https://flamee.vn/terms",
       lastModified,
       changeFrequency: "yearly",
       priority: 0.3,
     },
     {
-      url: "https://flamee.com/privacy",
+      url: "https://flamee.vn/privacy",
       lastModified,
       changeFrequency: "yearly",
       priority: 0.3,
