13e90ba6dc2d456ed2f2f99003e6afaec02cb1b5
thanhdev1710 <chithanh171004@gmail.com>
2025-04-19 11:47:21 +0700

    update url web

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,43 +5,43 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
   return [
     {
-      url: "https://flamee.vn",
+      url: "https://flamee-fe.vercel.app",
       lastModified,
       changeFrequency: "daily",
       priority: 1.0,
     },
     {
-      url: "https://flamee.vn/about",
+      url: "https://flamee-fe.vercel.app/about",
       lastModified,
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://flamee.vn/signin",
+      url: "https://flamee-fe.vercel.app/signin",
       lastModified,
       changeFrequency: "yearly",
       priority: 0.4,
     },
     {
-      url: "https://flamee.vn/signup",
+      url: "https://flamee-fe.vercel.app/signup",
       lastModified,
       changeFrequency: "yearly",
       priority: 0.4,
     },
     {
-      url: "https://flamee.vn/feed",
+      url: "https://flamee-fe.vercel.app/feed",
       lastModified,
       changeFrequency: "hourly",
       priority: 0.9,
     },
     {
-      url: "https://flamee.vn/terms",
+      url: "https://flamee-fe.vercel.app/terms",
       lastModified,
       changeFrequency: "yearly",
       priority: 0.3,
     },
     {
-      url: "https://flamee.vn/privacy",
+      url: "https://flamee-fe.vercel.app/privacy",
       lastModified,
       changeFrequency: "yearly",
       priority: 0.3,
