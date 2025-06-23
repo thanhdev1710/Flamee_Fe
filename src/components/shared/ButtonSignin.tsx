0e446f092c1d43a2b6a86bf29e9ce6613fdeffa7
thanhdev1710 <chithanh171004@gmail.com>
2025-06-23 16:33:51 +0700

    add github login

--- src/components/shared/ButtonSignin.tsx
+++ src/components/shared/ButtonSignin.tsx
@@ -3,23 +3,63 @@ import Image from "next/image";
 
 export default function ButtonSignin({ type }: { type: string }) {
   return (
-    <button
-      onClick={() => signIn("facebook")}
-      className="bg-black flex gap-2 items-center justify-center text-white cursor-pointer w-full py-2 rounded-lg"
-    >
-      <Image
-        alt="Logo Google"
-        src="/assets/images/gg.webp"
-        width={30}
-        height={30}
-      />
-      <span>
-        Hoặc{" "}
-        {type === "signin" || type === "send-reset-password"
-          ? "đăng nhập"
-          : "đăng ký"}{" "}
-        với Google
-      </span>
-    </button>
+    <div className="space-y-4">
+      <button
+        onClick={() => signIn("google")}
+        className="bg-black flex gap-2 items-center justify-center text-white cursor-pointer w-full py-2 rounded-lg"
+      >
+        <Image
+          alt="Logo Google"
+          src="/assets/images/gg.webp"
+          width={30}
+          height={30}
+        />
+        <span>
+          Hoặc{" "}
+          {type === "signin" || type === "send-reset-password"
+            ? "đăng nhập"
+            : "đăng ký"}{" "}
+          với Google
+        </span>
+      </button>
+      <div className="flex gap-4">
+        <button
+          onClick={() => signIn("facebook")}
+          className="bg-black flex gap-2 items-center justify-center text-white cursor-pointer w-full py-2 rounded-lg"
+        >
+          <Image
+            alt="Logo Facebook"
+            src="/assets/images/fb.webp"
+            width={30}
+            height={30}
+          />
+          <span>
+            Hoặc{" "}
+            {type === "signin" || type === "send-reset-password"
+              ? "đăng nhập"
+              : "đăng ký"}{" "}
+            với Facebook
+          </span>
+        </button>
+        <button
+          onClick={() => signIn("github")}
+          className="bg-black flex gap-2 items-center justify-center text-white cursor-pointer w-full py-2 rounded-lg"
+        >
+          <Image
+            alt="Logo Github"
+            src="/assets/images/github.webp"
+            width={30}
+            height={30}
+          />
+          <span>
+            Hoặc{" "}
+            {type === "signin" || type === "send-reset-password"
+              ? "đăng nhập"
+              : "đăng ký"}{" "}
+            với Github
+          </span>
+        </button>
+      </div>
+    </div>
   );
 }
