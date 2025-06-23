58309bb5d19b9a3612c21e699a5eda567682a311
thanhdev1710 <chithanh171004@gmail.com>
2025-06-23 16:06:52 +0700

    update login

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,25 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import Facebook from "next-auth/providers/facebook";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [Google, Facebook],
+  callbacks: {
+    async signIn({ user, account }) {
+      console.log({ user, account });
+
+      return false;
+    },
+    // async redirect({ url, baseUrl }) {
+    //   return baseUrl;
+    // },
+    // async session({ session, token, user }) {
+    //   return session;
+    // },
+    // async jwt({ token, user, account, profile, isNewUser }) {
+    //   return token;
+    // },
+  },
 });
 
 // TODO: LAM TIEP OW DAY
