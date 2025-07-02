2ac6881ae8b26b11599d1d40db5f5dc38dced16a
thanhdev1710 <chithanh171004@gmail.com>
2025-07-02 11:38:19 +0700

    update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -21,3 +21,9 @@ export async function withErrorHandler<T>(
 export function hexString(str: string) {
   return Buffer.from(str, "utf8").toString("hex");
 }
+
+export const formatNumber = (num: number) => {
+  if (num >= 1000000) return (num / 1000000).toFixed(1) + "M";
+  if (num >= 1000) return (num / 1000).toFixed(1) + "K";
+  return num.toString();
+};
