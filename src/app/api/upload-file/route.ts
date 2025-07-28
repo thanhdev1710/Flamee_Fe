1009e6c752fb2ad999e2c8b33566f7ac91cf2c0d
thanhdev1710 <chithanh171004@gmail.com>
2025-07-28 17:21:42 +0700

    update

--- src/app/api/upload-file/route.ts
+++ src/app/api/upload-file/route.ts
@@ -1,6 +1,12 @@
 import { compressImage } from "@/utils/compressImage";
 import { uploadToSpaces } from "@/utils/uploadToSpaces";
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 export async function POST(req: Request) {
   const uuid = crypto.randomUUID();
   const form = await req.formData();
