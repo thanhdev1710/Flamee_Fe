38f01082e8842ffb906cad29935acaeadd6ac445
thanhdev1710 <chithanh171004@gmail.com>
2025-11-20 10:27:15 +0700

    update conf

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
     ],
     dangerouslyAllowSVG: true,
   },
-  // output: "standalone",
+  output: "standalone",
   experimental: { serverActions: { bodySizeLimit: "10mb" } },
 };
 
